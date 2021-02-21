91582fd274a260e7e8d6d78477e6e9a199a65df6
ARTushar <1605070@ugrad.cse.buet.ac.bd>
2021-02-21 12:57:29 +0600

    add wishlist route

---
+++ pages/api/user/wishlist/[userid].js
@@ -0,0 +1,24 @@
+import verifyPremiumUser from "../../../../lib/middlewares/verifyPremiumUser";
+import verifyUser from "../../../../lib/middlewares/verifyUser";
+import { appendUserInWishlist, deleteUserFromWishlist, fetchUserWishlist } from "../../../../lib/utils/databaseInterection";
+import { createNC } from "../../../../lib/utils/ncHandlers";
+
+const handler = createNC();
+
+handler
+  .post(verifyUser, verifyPremiumUser, (req, res, next) => {
+    try {
+      const added = await appendUserInWishlist(req.user._id, req.query.userid);
+      res.status(200).json({ success: added })
+    } catch (err) {
+      next(err);
+    }
+  })
+  .delete(verifyUser, verifyPremiumUser, (req, res, next) => {
+    try {
+      const removed = await deleteUserFromWishlist(req.user._id, req.query.userid);
+      res.status(200).json({ success: added })
+    } catch (err) {
+      next(err);
+    }
+  })
--- pages/api/user/wishlist/index.js
+++ pages/api/user/wishlist/index.js
@@ -1,12 +1,18 @@
 import verifyPremiumUser from "../../../../lib/middlewares/verifyPremiumUser";
 import verifyUser from "../../../../lib/middlewares/verifyUser";
+import { fetchUserWishlist } from "../../../../lib/utils/databaseInterection";
 import { createNC } from "../../../../lib/utils/ncHandlers";
 
 const handler = createNC();
 
 handler
   .get(verifyUser, verifyPremiumUser, (req, res, next) => {
-    
+    try {
+      const wishList = await fetchUserWishlist(req.user._id);
+      req.status(200).json(wishList);
+    } catch (error) {
+      next(error);
+    }
   })
 
 export default handler;
