45e0f5af91f400d45485e32e14da74f8b65c315e
ARTushar <1605070@ugrad.cse.buet.ac.bd>
2021-02-26 16:16:57 +0600

    implement received requests routes

--- pages/api/user/receivedrequests/[requestid].js
+++ pages/api/user/receivedrequests/[requestid].js
@@ -1,11 +1,17 @@
 import verifyUser from "../../../../lib/middlewares/verifyUser";
+import { changeRequestStatus } from "../../../../lib/utils/databaseInterection";
 import { createNC } from "../../../../lib/utils/ncHandlers";
 
 const handler = createNC();
 
 handler
-  .post(verifyUser, (req, res, next) => {
-
+  .put(verifyUser, (req, res, next) => {
+    try {
+      const changed = await changeRequestStatus(req.query.requestId, req.body);
+      res.status(200).json({ changed: changed });
+    } catch (error) {
+      next(error);
+    }
   });
 
 export default handler;
--- pages/api/user/receivedrequests/index.js
+++ pages/api/user/receivedrequests/index.js
@@ -1,11 +1,17 @@
 import verifyUser from "../../../../lib/middlewares/verifyUser";
+import { fetchReceivedRequests } from "../../../../lib/utils/databaseInterection";
 import { createNC } from "../../../../lib/utils/ncHandlers";
 
 const handler = createNC();
 
 handler
   .get(verifyUser, (req, res, next) => {
-
+    try {
+      const requests = await fetchReceivedRequests(req.user._id);
+      res.status(200).json(requests);
+    } catch (error) {
+      next(error);
+    }
   });
 
 export default handler;
