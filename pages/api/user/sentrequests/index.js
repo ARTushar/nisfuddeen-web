f7cd7bc144b73214e29b494cebd5ec99618e55ab
ARTushar <1605070@ugrad.cse.buet.ac.bd>
2021-02-26 16:29:01 +0600

    implement sent requests route

--- pages/api/user/sentrequests/index.js
+++ pages/api/user/sentrequests/index.js
@@ -1,12 +1,18 @@
 import verifyPremiumUser from "../../../../lib/middlewares/verifyPremiumUser";
 import verifyUser from "../../../../lib/middlewares/verifyUser";
+import { fetchSentRequests } from "../../../../lib/utils/databaseInterection";
 import { createNC } from "../../../../lib/utils/ncHandlers";
 
 const handler = createNC();
 
 handler
   .get(verifyUser, verifyPremiumUser, (req, res, next) => {
-
+    try {
+      const requests = await fetchSentRequests(req.user._id);
+      res.json(200).status(requests);
+    } catch (error) {
+      next(error);
+    }
   });
 
 export default handler;
