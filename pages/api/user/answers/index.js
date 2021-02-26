3fbb262402a727f723c342a696958a66c19221ca
ARTushar <1605070@ugrad.cse.buet.ac.bd>
2021-02-26 19:46:13 +0600

    implement answers route

--- pages/api/user/answers/index.js
+++ pages/api/user/answers/index.js
@@ -1,20 +1,41 @@
 import verifyUser from "../../../../lib/middlewares/verifyUser";
+import { fetchAnswers, submitBiodata, updateBiodata, invisibleBiodata } from "../../../../lib/utils/databaseInterection";
 import { createNC } from "../../../../lib/utils/ncHandlers";
 
 const handler = createNC();
 
 handler
   .get(verifyUser, (req, res, next) => {
-
+    try {
+      const answers = fetchAnswers(req.user._id);
+      res.status(200).json(answers);
+    } catch (error) {
+      next(error);
+    }
   })
   .post(verifyUser, (req, res, next) => {
-
+    try {
+      const biodata = await submitBiodata(req.user._id, req.body) 
+      res.status(200).json(biodata);
+    } catch (error) {
+      next(error)
+    }
   })
   .put(verifyUser, (req, res, next) => {
-
+    try {
+      const updatedBiodata = await updateBiodata(req.user._id, req.body);
+      res.status(200).json(updatedBiodata);
+    } catch (error) {
+      next(error);
+    }
   })
   .delete(verifyUser, (req, res, next) => {
-
+    try {
+      const removed = await invisibleBiodata(req.user._id);
+      res.status(200).json({ removed: removed });
+    } catch (error) {
+      next(error);
+    }
   });
 
 export default handler;
