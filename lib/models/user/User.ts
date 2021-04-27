e44092d2c2132264315f0644896626d435ae0c9f
ARTushar <1605070@ugrad.cse.buet.ac.bd>
2021-04-27 11:23:07 +0600

    refactor code

--- lib/models/user/User.ts
+++ lib/models/user/User.ts
@@ -1,4 +1,10 @@
 import { AccountType, SubscriptionType } from '../../Types/types';
+import { accountTypeFactory } from '../../Types/factoryTypes';
+import createUser from '../../dataAccessLayer/entities/user/createUser';
+import { errorFactory } from '../../utils/helpers';
+import { createServerError } from '../../utils/errorCreators';
+import { getUserByEmail, getUserByMobile } from '../../dataAccessLayer/entities/user/getUser';
+import { comparePassword } from '../../utils/passwordHelpers';
 
 interface UserConstructorParams {
     userId?: string;
@@ -12,7 +18,7 @@ interface UserConstructorParams {
     updatedAt?: string;
 }
 
-export class User {
+export default class User {
     userId: string;
     fullName: string;
     mobileNumber: string;
@@ -32,5 +38,42 @@ export class User {
         this.accountType = accountType;
         this.subscriptionType = subscriptionType;
     }
-    
+
+
+    static async createAccount(fullName: string, email: string, mobile: string, accountType: string, password: string): Promise<User> {
+        const ac = accountTypeFactory(accountType);
+
+        const user: User = await createUser(new User({
+            fullName: fullName,
+            mobileNumber: mobile,
+            email: email,
+            password: password,
+            accountType: ac
+        }));
+
+        if(user) return user;
+        throw errorFactory(new Error("Cannot Create the account."), createServerError);
+    }
+
+    static async loginByEmail(email: string, password: string) {
+        const user: User = await getUserByEmail(email);
+        if(!user) {
+            throw errorFactory(new Error("Wrong Email"), createServerError);
+        }
+        if(! await comparePassword(password, user.password)){
+            throw errorFactory(new Error("Wrong Password"), createServerError);
+        }
+        return user;
+    }
+
+    static async loginByMobile(mobile: string, password: string) {
+        const user: User = await getUserByMobile(mobile);
+        if(!user) {
+            throw errorFactory(new Error("Wrong Mobile"), createServerError);
+        }
+        if(! await comparePassword(password, user.password)){
+            throw errorFactory(new Error("Wrong Password"), createServerError);
+        }
+        return user;
+    }
 }
